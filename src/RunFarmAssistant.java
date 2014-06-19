b59f8da101dd5ece27d7ca539ec8ac0bfff3ab5b
cozos <arwin.tio@hotmail.com>
2014-06-18 22:39:05 -0400

    Update RunFarmAssistant.java

--- src/RunFarmAssistant.java
+++ src/RunFarmAssistant.java
@@ -25,10 +25,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 @RunWith(JUnit4.class)
 public class RunFarmAssistant extends TestCase {
 
+	private static final String WINDOWS32_CHROMEPATH = "chromedriver_win32\\chromedriver.exe";
+	private static final String LINUX64_CHROMEPATH   = "chromedriver_linux64/chromedriver";
+	private static final String USERNAME 			 = "SmallJohnson";
+	private static final String PASSWORD			 = "33333";
+	private static final int FARMINTERVAL_MILS		 = 10000000;
+	
 	private static ChromeDriverService service;
 	private WebDriver driver;
-	private static final String WINDOWS32_CHROMEPATH = "chromedriver_win32\\chromedriver.exe";
-	private static final string LINUX64_CHROMEPATH = "chromedriver_linux64/chromedriver";
 	
 	@BeforeClass
 	public static void createAndStartService() throws IOException {
@@ -61,10 +65,10 @@ public class RunFarmAssistant extends TestCase {
 		waitForLoad(driver);
 
 		WebElement loginName = driver.findElement(By.id("user"));
-		loginName.sendKeys("SmallJohnson");
+		loginName.sendKeys(USERNAME);
 
 		WebElement loginPassword = driver.findElement(By.id("password"));
-		loginPassword.sendKeys("33333");
+		loginPassword.sendKeys(PASSWORD);
 
 		List<WebElement> loginButton = driver.findElements(By.className("login_button"));
 		loginButton.get(0).click();
@@ -88,43 +92,60 @@ public class RunFarmAssistant extends TestCase {
 
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.className("farm_icon_b ")));
 
-		int lightCavToSend = Integer.parseInt(driver.findElements(By.name("light")).get(1).getAttribute("value"));
-		int lightCavRemaining = Integer.parseInt(driver.findElement(By.id("light")).getAttribute("textContent"));
-
-		System.out.println("toSend = " + lightCavToSend + " Remaining = " + lightCavRemaining);
-
-		List<WebElement> tbody = driver.findElements(By.tagName("tbody"));
-		List<WebElement> trList = tbody.get(tbody.size()-1).findElements(By.tagName("tr"));
-		List<WebElement reportList = trList.subList(2,trList.size()-1);
-
-		List<WebElement> farmButtons = driver.findElements(By.className("farm_icon_b"));
-		farmButtons = farmButtons.subList(1,farmButtons.size());
+		int pageNum = Integer.parseInt(driver.findElements(By.tagName("Strong")).get(0).substring(2,3)) - 1;
+		int numPages = driver.findElements(By.className("paged-nav-item")).size();
+		boolean select = driver.findElement(By.id("select")) != null;
 		
-		assertTrue(farmButtons.size() == reportList.size());
-		
-		for (int i = 0; i < farmButtons.size(); i++) {
-			List<WebElement> tdList = reportList[i].findElements(By.tagName("td"));
-			Barb farm = new Barb(tdList[3].getAttribute("textContent").substring(2,5),tdList[3].getAttribute("textContent").substring(6,9));
-			boolean hasAttacked = tdList[3].findElements(By.tagName("img")).size() != 0;
-			boolean isGreen = tdList[1].findElements(By.tagName("img")).getAttribute("src").indexOf("green.png") != -1;
-			System.out.println(farm.x + "@" + farm.y);
+		for(int i = 0; i < numPages; i++){
+			List<WebElement> pageNavItems = driver.findElements(By.className("paged-nav-item"));
 			
-			if ( isGreen ){
-				// Remove from walled
+			int lightCavToSend = Integer.parseInt(driver.findElements(By.name("light")).get(1).getAttribute("value"));
+			int lightCavRemaining = Integer.parseInt(driver.findElement(By.id("light")).getAttribute("textContent"));
+	
+			System.out.println("toSend = " + lightCavToSend + " Remaining = " + lightCavRemaining);
+			System.out.println("pageNum = " + pageNum + " numPages = " + numPages);
+			
+			List<WebElement> tbody = driver.findElements(By.tagName("tbody"));
+			List<WebElement> trList = tbody.get(tbody.size()-1).findElements(By.tagName("tr"));
+			List<WebElement reportList = trList.subList(2,trList.size()-1);
+	
+			List<WebElement> farmButtons = driver.findElements(By.className("farm_icon_b"));
+			farmButtons = farmButtons.subList(1,farmButtons.size());
+			
+			assertTrue(farmButtons.size() == reportList.size());
+			
+			for (int i = 0; i < farmButtons.size(); i++) {
+				List<WebElement> tdList = reportList[i].findElements(By.tagName("td"));
+				Barb farm = new Barb(tdList[3].getAttribute("textContent").substring(2,5),tdList[3].getAttribute("textContent").substring(6,9));
+				boolean hasAttacked = tdList[3].findElements(By.tagName("img")).size() != 0;
+				boolean isGreen = tdList[1].findElements(By.tagName("img")).getAttribute("src").indexOf("green.png") != -1;
+				System.out.println(farm.x + "@" + farm.y);
+				
+				long travelTime         = Long.parseLong(tdList[7].getAttribute("textContent")) * 8 * 60;
+				Date currentLandingTime = Calender.getInstance().getTime() + new Date((int)travelTime);
+				
+				if ( isGreen ){
+					// Remove from walled
+				}
+				if( !isGreen){
+					// Add to walled
+				}
+				else if ( hasAttacked){
+					// Check if its not too early to attack
+				}
+				else{
+					// Record currentLandingTime
+					farmButtons[i].click();
+					lightCavRemaining -= lightCavToSend;
+				}
+				
+				if(lightCavRemaining < lightCavToSend) break;
 			}
-			if( !isGreen){
-				// Add to walled
-			}
-			else if ( hasAttacked){
-				// Check if its not too early to attack
-			}
-			else{
-				long travelTime = Long.parseLong(tdList[7].getAttribute("textContent")) * 8 * 60;
-				farmButtons[i].click();
-				// Record landing time
-			}
+			if(lightCavRemaining < lightCavToSend) break;
+			pageNavItems[pageNum].click();
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.className("farm_icon_b ")));
+			pageNum++;
 		}
-
 		Thread.sleep(100000);
 	}
 
